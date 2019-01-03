8c909f10abbfe9e6185505c83aea823238a5b542
Ron Arbel <ronarbel1@gmail.com>
2019-01-02 21:39:37 -0800

    render all 3 available components

--- server/index.js
+++ server/index.js
@@ -5,17 +5,6 @@ const axios = require('axios');
 const app = express();
 
 app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
-app.use(bodyParser());
-
-app.get('/buildPage/restaurants/:id/', (req, res) => {
-  axios.get(`http://localhost:3001/api/reservations/bundle/`)
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(err => {
-      throw err;
-    });
-});
 
 const port = 3000;
 app.listen(port, () => {
