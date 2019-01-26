873beaa6e29cc5fbb9aefb44dc40ab3d9e0e993a
Connie Ea <connieyeah@gmail.com>
2019-01-25 23:21:15 -0800

    proxy WIP

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
-import $ from 'jquery';
-const axios = require('axios');
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+// import $ from 'jquery';
+// const axios = require('axios');
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     };
+//   }
 
-  render() {
-    return (
-      <div>
-        Hello World 2
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         Hello World 2
+//       </div>
+//     );
+//   }
+// }
 
-if (typeof window !== 'undefined') {
-  ReactDOM.render(<App />, document.getElementById('app'));
-}
+// if (typeof window !== 'undefined') {
+//   ReactDOM.render(<App />, document.getElementById('app'));
+// }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const cors = require('cors');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser());
+
 app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
 
-const port = 3000;
+app.use('/api/restaurants/', proxy({ target: 'http://3.16.186.40:3003', changeOrigin: true}));
+
+// app.get('/restaurants/:id/menu', (req, res) => {
+//   // get all the menu items from the menu server
+//   axios.get(`http://ec2-3-16-186-40.us-east-2.compute.amazonaws.com:3003/restaurants/${req.params.id}/menu`)
+//   .then(function (req) {
+//     res.status(200).send(req.data);
+//   })
+//   .catch(function (error) {
+//     res.status(501).send(error);
+//   });
+// });
+
+const port = 1337;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+
